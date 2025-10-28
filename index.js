3b15df07a75ddea4825ff8a022f4e300ab4d3bc9
irinakiseleva0 <irina.kiseleva@epita.fr>
2025-10-28 14:59:00 +0100

    Initial commit - setup Express server

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+
+
+const app = express();
+
+
+const PORT = 3000;
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({
+        firstName: "John",
+        lastName: "Doe",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
