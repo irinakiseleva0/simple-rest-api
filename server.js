afadfc0cadb7008e959ef9dd5cca66bbfcb86f26
irinakiseleva0 <irina.kiseleva@epita.fr>
2025-11-16 18:21:02 +0100

    Fix server and enable CORS

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: "*",
+    allowedHeaders: ["Content-Type", "X-API-Key"],
+}));
 
 app.get("/", (req, res) => {
     res.send("Hello from Express!");
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
